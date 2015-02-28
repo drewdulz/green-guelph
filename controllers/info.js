338c1643094f7c04fc2f99bafac1c6eab3b5a973
Drew Warkentin <drewdulz@gmail.com>
2015-02-28 15:30:46 -0500

    javascript functionality

--- controllers/info.js
+++ controllers/info.js
@@ -1,29 +1,44 @@
 //Set this data to be reacitve then pull it into the front end and display it.
 //We can have some data that does describes how recycling works and what it is recycled into.
 
-var info = {
-	dairy: {
+var info = [
+	{
 		name: "Dairy Products",
 		notes: "",
 		image: "/imagepath"
 	}, 
-	food: {
+	{
 		name: "Food Scraps",
 		notes: "",
 		image: "/imagepath"
 	},
-	fruits: {
+	{
 		name: "Fruits",
 		notes: "",
 		image: "/imagepath"
 	},
-	grain: {
+	{
 		name: "Grain Products",
 		notes: "",
 		image: "/imagepath"
 	}
 	//Continue on here
-}
+]
 
 console.log(info);
+
+if (Meteor.isClient) {
+	Template.infoCategories.helpers({
+    info: function () {
+      return Session.get("info");
+      // Session.set("info", info);
+
+    },
+  });
+
+  Template.infoCategories.rendered = function(){
+  	Session.set("info", info);
+	};
+
+}
 
